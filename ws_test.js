de5ddb766355dfe851a7e1e61ac046f64c82eb84
FeelMySkin <zetsuboulevel@gmail.com>
2018-10-23 16:03:54 +0300

    ws_created

---
+++ ws_test.js
@@ -0,0 +1,54 @@
+var wsPort = 3389;
+
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+var clients = [];
+
+var server = http.createServer(function(request,response)
+{
+
+});
+
+server.listen(wsPort, function()
+{
+	console.log("server listening on port " + wsPort);
+});
+
+var wsServer = new WebSocketServer({
+	httpServer: server
+});
+
+wsServer.on('request', function(request)
+{
+	console.log('Connected from ' + request.origin + '.');
+
+	var connection = request.accept(null, request.origin);
+})
+
+var io = require('socket.io')(3389);
+var counter = 0;
+
+io.on('connection', function(socket)
+{
+	console.log("user connected "  + socket.id);
+	
+	socket.on('increase', function(anon)
+	{
+		counter++;
+		console.log("Increased " + socket.id);
+		if(!anon) io.emit('changed',counter);
+		else io.to(socket.id).emit('changed',counter);
+	});
+	
+	socket.on('decrease',function()
+	{
+		counter--;
+		console.log("Decreased " + socket.id);
+		io.emit('changed',counter);
+	});
+	
+	socket.on('disconnect',function()
+	{
+		console.log("User disconnected " +socket.id);
+	});
+});
