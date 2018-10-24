04521c88ff0a38894a42bdea71a9520fea256602
FeelMySkin <zetsuboulevel@gmail.com>
2018-10-24 12:59:25 +0300

    ASP

--- dotNET_test/Startup.cs
+++ dotNET_test/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.WebSockets;
 
 namespace dotNET_test
 {
@@ -17,18 +18,58 @@ namespace dotNET_test
         {
         }
 
+        private async Task Echo(HttpContext context, WebSocket socket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await socket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await socket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+            var webSocketOptions = new WebSocketOptions()
+            {
+                KeepAliveInterval =TimeSpan.FromSeconds(5);
+                ReceiveBufferSize = 4096;
+            }
+
+            app.UseWebSockets();
+
+            app.Use(async (context, next) =>
+            {
+                if(context.Request.Path == ":3389")
+                {
+                    if(context.WebSockets.IsWebSocketRequest)
+                    {
+                        WebSocket ws = await context.WebSockets.AcceptWebSocketAsync();
+                        
+                    }
+                    else context.Response.StatusCode = 400;
+                }
+                else
+                {
+                    await next();
+                }
+            });
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            /* app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
-            });
+            });*/
         }
     }
 }
