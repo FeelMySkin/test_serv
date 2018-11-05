c3d46f962eb4e09f89f63b9e710fed6a773130a8
FeelMySkin <zetsuboulevel@gmail.com>
2018-11-05 22:17:58 +0300

    none

--- promise_test.js
+++ promise_test.js
@@ -3,9 +3,13 @@ const Crypto = require('crypto');
 var hash = Crypto.createHash('sha256');
 hash.write('tst');
 
-hash.on('ready',function(err,res)
+console.log("Start");
+
+
+hash.on('readable',function()
 {
     console.log('hash: ' + hash.read().toString('hex'));
 });
 
 hash.end();
+console.log("End");
