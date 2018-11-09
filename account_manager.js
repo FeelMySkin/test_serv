26134dbfcbced08fcd5504a60a78cb110af7773a
FeelMySkin <zetsuboulevel@gmail.com>
2018-11-09 23:25:39 +0300

    account_mgmt with promises

--- account_manager.js
+++ account_manager.js
@@ -17,17 +17,30 @@ const db_client = new Client({
 var k_client = new kafka.KafkaClient({kafkaHost: 'localhost:9092'});
 var producer = new kafka.Producer(k_client);
 var consumer = new kafka.Consumer(k_client,[]);
-
-
+const topic = 'account_topic';
 
 
 db_client.connect();
-k_client.createTopics([{topic: 'test_topic', partitions:2, replicationFactor:1}],(error,result) => 
+
+
+function SendToKafka(json)
+{
+	
+	var payload = [{topic: topic, messages: [json], partition: 1, timestamp: Date.now()}];
+	producer.send(payload,function(err,data)
+	{
+		console.log("payload sent:");
+		console.log(err);
+		console.log(data);
+	});
+}
+
+k_client.createTopics([{topic: topic, partitions:2, replicationFactor:1}],(error,result) => 
 {
 	console.log("Topic creation:");
 	console.log(error);
 	console.log(result);
-	consumer.addTopics([{topic: 'test_topic', partition:0}], function (err,result)
+	consumer.addTopics([{topic: topic, partition:0}], function (err,result)
 	{
 		console.log("Topic added:");
 		console.log(err);
@@ -35,13 +48,78 @@ k_client.createTopics([{topic: 'test_topic', partitions:2, replicationFactor:1}]
 	});
 });
 
+function GetFromDB(table_name,columns,conditions = null)
+{
+	var query = "SELECT " + columns + " FROM " + table_name;
+	if(conditions != null) query += " WHERE " + conditions + ';';
+	else query += ';';
+	
+	return new Promise(function(resolve,reject)
+	{
+		db_client.query(query,function(err,res)
+		{
+			resolve(res);
+		});
+	});
+
+}
+
+function InsertToDB(table_name, properties)
+{
+	var prop_name = "(";
+	var prop_field = "(";
+
+	for (x in properties)
+	{
+		prop_name += x + ', ';
+		prop_field += properties[x] + ', ';
+	}
+
+	prop_name = prop_name.slice(0,prop_name-2) + ')';
+	prop_field = prop_field.slice(0,prop_field-2) + ')';
+
+	var query = "INSERT INTO " + table_name + ' ' + prop_name + " VALUES " + prop_field + ';';
+
+	return new Promise(function(resolve, reject)
+	{
+		db_client.query(query,function(err,res)
+		{
+			resolve(res);
+		});
+	});
+}
+
+function Register(json)
+{
+	/*JSON file: signal, mail, password_hash, username, socket_id*/
+	GetFromDB('account_test','*','mail=' + json.mail)
+	.then(function(res)
+	{
+		var backpack;
+		if(res.rowCount == 0) backpack = JSON.stringify({signal:'register', socket_id: json.socket_id, status:true, message:'Success'});
+		else backpack = JSON.stringify({signal:'register', socket_id: json.socket_id, status:true, message:'Success'});
+
+		SendToKafka(backpack);
+		if(res.rowCount == 0) resolve(1);
+		else reject(1);
+	})
+	.then(function(res)
+	{
+		InsertToDB('account_test',{mail:json.mail, password_hash:json.password_hash, username: json.username});
+	});
+}
+
 consumer.on("message",function(mess)
 {
 	console.log(mess);
-	var recv = mess.value;
-	var json = JSON.parse(recv);
+	var json = JSON.parse(mess.value);
 	var payload;
-	console.log(json);
+
+	switch (json.signal)
+	{
+		case 'register':
+			Register(json);
+	}
 
 	db_client.query("SELECT * FROM account_test WHERE mail='" + json.mail + "';", function(err,res)
 	{
@@ -49,8 +127,8 @@ consumer.on("message",function(mess)
 		console.log(err);
 		console.log(res);
 		console.log(res.length);
-		if(res.rowCount == 0) payload = [{topic: 'test_topic', messages: [JSON.stringify({status: true})], partition: 1, timestamp: Date.now()}];
-		else  payload = [{topic: 'test_topic', messages: [JSON.stringify({status: false})], partition: 1, timestamp: Date.now()}];
+		if(res.rowCount == 0) payload = [{topic: topic, messages: [JSON.stringify({status: true})], partition: 1, timestamp: Date.now()}];
+		else  payload = [{topic: topic, messages: [JSON.stringify({status: false})], partition: 1, timestamp: Date.now()}];
 
 		producer.send(payload,function(err,res)
 		{
---
+++ account_server.js
@@ -0,0 +1,85 @@
+const io = require('socket.io')(3389);
+const kafka = require('kafka-node');
+const crypto = require ('crypto');
+
+var k_client = new kafka.KafkaClient({kafkaHost: 'localhost:9092'});
+var producer = new kafka.Producer(k_client);
+var consumer = new kafka.Consumer(k_client,[]);
+
+var topic = 'account_topic';
+
+k_client.createTopics([{topic: topic, partitions:2, replicationFactor:1}],(error,result) => 
+{
+	console.log("Topic creation:");
+	console.log(error);
+	console.log(result);
+	consumer.addTopics([{topic: topic, partition:1}], function (err,result)
+	{
+		console.log("Topic added:");
+		console.log(err);
+		console.log(result);
+	});
+});
+
+consumer.on('message',function(mess)
+{
+    console.log(mess);
+    var unpacked = JSON.parse(mess)
+    {
+        switch(unpacked.signal)
+        {
+            case 'register':
+                var backpacked = JSON.stringify({status:unpacked.status, message:unpacked.message})
+                io.to(unpacked.socket_id).emit(backpacked);
+                break;
+
+            default:
+                break;
+        }
+    }
+});
+
+function SendToKafka(json)
+{
+	
+	var payload = [{topic: topic, messages: [json], partition: 0, timestamp: Date.now()}];
+	producer.send(payload,function(err,data)
+	{
+		console.log("payload sent:");
+		console.log(err);
+		console.log(data);
+	});
+}
+
+function CountHash(text)
+{
+    return new Promise(function(resolve,reject)
+    {
+        var hash = Crypto.createHash("sha256");
+        hash.write(text);
+        hash.end();
+
+        hash.on('readable',function()
+        {
+            resolve(hash.read());
+        });
+    });
+}
+
+io.on('connection', function(socket)
+{
+	console.log("user connected "  + socket.id);
+
+	socket.on('register', function(json)
+	{
+        var unpacked = JSON.parse(json);
+		console.log('acc create received: \nmail: ' + unpacked.mail + ";\npass: " + unpacked.pass +";\nusername: " + unpacked.username);
+        
+        CountHash(unpacked.pass)
+        .then( (hash) =>
+            {
+                var backpacked = JSON.stringify({signal: 'register', mail:mail, password_hash: hash,username:username, socket_id: socket.id});
+                SendToKafka(backpacked);
+            });
+	});
+});
